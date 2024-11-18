d0da078c3046e65862861671c9f7d92622a9862d
Samson Oyede <oyedetobi49@gmail.com>
2024-11-18 21:11:21 +0000

    Conver text to audio

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
 from flask import Flask, request, jsonify
-import os
 from helper import *
+
 
 app = Flask(__name__)
 
-# Configure upload folder
+
+
 BASE_DIRECTORY = 'uploads'
+
+AUDIO_BASE_DIRECTORY = 'audio'
 
 
 os.makedirs(BASE_DIRECTORY, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = BASE_DIRECTORY
+os.makedirs(AUDIO_BASE_DIRECTORY, exist_ok=True)
+
+
 
 @app.route('/')
 def index():
-    return  jsonify({'Message': 'Hello TextToAudio'})
+    return  jsonify({'Message': 'Hello TextToAudio API'})
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # Check if file part is in the request
     if 'file' not in request.files:
-        return jsonify({'message': 'no file in request'}), 200
+        return jsonify({'message': 'no file in request'}), 400
 
     f = request.files['file']
     
@@ -29,24 +35,25 @@ def upload_file():
     
     # Save the file
     f.save(os.path.join(BASE_DIRECTORY, f.filename))
-    return jsonify({'message': 'File uploaded successfully'})
+    return jsonify({'message': 'File uploaded successfully'}), 200
 
 
 @app.route('/content', methods=['GET'])
 def get_file():
 
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    files = os.listdir(BASE_DIRECTORY)
     content = ''
-    
+
     for file in files:
         file_path = f'{BASE_DIRECTORY}/{file}'
-        print(file_path)
+        audio_file_url = f'{AUDIO_BASE_DIRECTORY}/{file}.mp3'
+
         file_content = read_file_content(file_path)
+        convert_text_to_speech(file_content,audio_file_url)
+
         content +=  file_content
-
+    
     return jsonify({'message': f'The content of the file is  - {content}'}), 200
-
-
 
 
 if __name__ == "__main__":
--- helper.py
+++ helper.py
@@ -1,5 +1,27 @@
+from dotenv import load_dotenv
+import os
+import openai
+load_dotenv()
+
+openai.api_key = os.getenv('API_KEY')
+
 
 def read_file_content(file):
     with open(file, 'r') as f:
         content = f.read()
         return content
+    
+
+
+def convert_text_to_speech(content, file_url):
+    response = openai.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=content
+    )
+    with open(file_url, 'wb') as audio_file:
+        audio_file.write(response.content)
+
+
+
+    
